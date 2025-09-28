63f246f76b0df921c77878d9451d2aa28c5e8cd1
Mincho Minchev <minchominchev91@hotmail.com>
2025-09-28 16:39:02 +0300

    function name

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_service_plan" "plan" {
 
 # Function App
 resource "azurerm_linux_function_app" "func" {
-  name                       = "sf-mntr-func"
+  name                       = "snowflake-monitoring-func"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
   service_plan_id            = azurerm_service_plan.plan.id
